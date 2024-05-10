e2f0604fc0f7ff93d83bce254c67301116abb95f
The coolest farley <surelyilikemen@gmail.com>
2024-05-10 17:33:52 -0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -83,7 +83,7 @@ function Library:tween(object, goal, callback)
 	tween:Play()
 end
 
-function Library:Window( args )
+function Library:Window(args)
 	-- Check if args are correct
 	args = Library:Validate({
 		Title = "Title"
@@ -91,28 +91,34 @@ function Library:Window( args )
 
 	-- Vars 
 	local This = {
-        CurrentTab = nil
-    }
+		CurrentTab = nil
+	}
 	local Minimized = false
 
 	-- Instancing
 	local MainFrame = Instance.new("Frame")
-    local MainFrameUiCorner = Instance.new("UICorner")
-    local Topbar = Instance.new("Frame")
-    local TopbarUiCorner = Instance.new("UICorner")
-    local Title = Instance.new("TextLabel")
-    local MinimizeButton = Instance.new("ImageButton")
-    local CloseButton = Instance.new("ImageButton")
-    local Navigation = Instance.new("Frame")
-    local TemplateTabButton = Instance.new("TextButton")
-    local TabButtonImage = Instance.new("ImageLabel")
-    local TabButtonUiCorner = Instance.new("UICorner")
-    local UIListLayout = Instance.new("UIListLayout")
-    local TabHolderUiCorner = Instance.new("UICorner")
-    local DropShadowHolder = Instance.new("Frame")
-    local DropShadow = Instance.new("ImageLabel")
-    local Tabholder = Instance.new("Frame")
-    local Template = Instance.new("Frame")
+	local MainFrameUiCorner = Instance.new("UICorner")
+	local Topbar = Instance.new("Frame")
+	local TopbarUiCorner = Instance.new("UICorner")
+	local Title = Instance.new("TextLabel")
+	local MinimizeButton = Instance.new("ImageButton")
+	local CloseButton = Instance.new("ImageButton")
+	local Navigation = Instance.new("Frame")
+	local TemplateTabButton = Instance.new("TextButton")
+	local TabButtonImage = Instance.new("ImageLabel")
+	local TabButtonUiCorner = Instance.new("UICorner")
+	local UIListLayout = Instance.new("UIListLayout")
+	local TabHolderUiCorner = Instance.new("UICorner")
+	local DropShadowHolder = Instance.new("Frame")
+	local DropShadow = Instance.new("ImageLabel")
+	local Tabholder = Instance.new("Frame")
+	local Template = Instance.new("ScrollingFrame")
+	local TemplateButton = Instance.new("Frame")
+	local ButtonUICorner = Instance.new("UICorner")
+	local Detection = Instance.new("TextButton")
+	local DetectionUICorner = Instance.new("UICorner")
+	local ButtonImage = Instance.new("ImageLabel")
+	local ButtonImageUiCorner = Instance.new("UICorner")
 
 	MainFrame.Name = "MainFrame"
 	MainFrame.Parent = ScreenGui
@@ -194,32 +200,32 @@ function Library:Window( args )
 	TabHolderUiCorner.Parent = Navigation
 
 	TemplateTabButton.Name = "TemplateTabButton"
-    TemplateTabButton.Parent = Navigation
-    TemplateTabButton.BackgroundColor3 = Color3.fromRGB(72, 72, 72)
-    TemplateTabButton.BackgroundTransparency = 1.000
-    TemplateTabButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    TemplateTabButton.BorderSizePixel = 0
-    TemplateTabButton.Size = UDim2.new(1, 0, 0.0833780989, 0)
-    TemplateTabButton.Visible = false
-    TemplateTabButton.Font = Enum.Font.Gotham
-    TemplateTabButton.Text = "Tab"
-    TemplateTabButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-    TemplateTabButton.TextSize = 14.000
+	TemplateTabButton.Parent = Navigation
+	TemplateTabButton.BackgroundColor3 = Color3.fromRGB(72, 72, 72)
+	TemplateTabButton.BackgroundTransparency = 1.000
+	TemplateTabButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TemplateTabButton.BorderSizePixel = 0
+	TemplateTabButton.Size = UDim2.new(1, 0, 0.0833780989, 0)
+	TemplateTabButton.Visible = false
+	TemplateTabButton.Font = Enum.Font.Gotham
+	TemplateTabButton.Text = "Tab"
+	TemplateTabButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+	TemplateTabButton.TextSize = 14.000
 
-    TabButtonImage.Name = "TabButtonImage"
-    TabButtonImage.Parent = TemplateTabButton
-    TabButtonImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    TabButtonImage.BackgroundTransparency = 1.000
-    TabButtonImage.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    TabButtonImage.BorderSizePixel = 0
-    TabButtonImage.Position = UDim2.new(0, 0, 0.200000003, 0)
-    TabButtonImage.Size = UDim2.new(0, 20, 0, 14)
-    TabButtonImage.Image = "rbxassetid://13848371774"
-    TabButtonImage.ScaleType = Enum.ScaleType.Fit
+	TabButtonImage.Name = "TabButtonImage"
+	TabButtonImage.Parent = TemplateTabButton
+	TabButtonImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	TabButtonImage.BackgroundTransparency = 1.000
+	TabButtonImage.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TabButtonImage.BorderSizePixel = 0
+	TabButtonImage.Position = UDim2.new(0, 0, 0.200000003, 0)
+	TabButtonImage.Size = UDim2.new(0, 20, 0, 14)
+	TabButtonImage.Image = "rbxassetid://13848371774"
+	TabButtonImage.ScaleType = Enum.ScaleType.Fit
 
-    TabButtonUiCorner.CornerRadius = UDim.new(0, 2)
-    TabButtonUiCorner.Name = "TabButtonUiCorner"
-    TabButtonUiCorner.Parent = TemplateTabButton
+	TabButtonUiCorner.CornerRadius = UDim.new(0, 2)
+	TabButtonUiCorner.Name = "TabButtonUiCorner"
+	TabButtonUiCorner.Parent = TemplateTabButton
 
 	UIListLayout.Parent = Navigation
 	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
@@ -256,12 +262,56 @@ function Library:Window( args )
 
 	Template.Name = "Template"
 	Template.Parent = Tabholder
-	Template.AnchorPoint = Vector2.new(0.5, 0.5)
+	Template.Active = true
 	Template.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 	Template.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	Template.BorderSizePixel = 0
-	Template.Position = UDim2.new(0.5, 0, 0.5, 0)
 	Template.Size = UDim2.new(1, 0, 1, 0)
+	Template.Visible = false
+	Template.ScrollBarThickness = 2
+
+	TemplateButton.Name = "TemplateButton"
+	TemplateButton.Parent = Template
+	TemplateButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+	TemplateButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	TemplateButton.BorderSizePixel = 0
+	TemplateButton.Size = UDim2.new(0, 180, 0, 30)
+	TemplateButton.Visible = false
+
+	ButtonUICorner.CornerRadius = UDim.new(0, 2)
+	ButtonUICorner.Name = "ButtonUICorner"
+	ButtonUICorner.Parent = TemplateButton
+
+	Detection.Name = "Detection"
+	Detection.Parent = TemplateButton
+	Detection.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Detection.BackgroundTransparency = 1.000
+	Detection.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	Detection.BorderSizePixel = 0
+	Detection.Size = UDim2.new(1, 0, 1, 0)
+	Detection.Font = Enum.Font.Gotham
+	Detection.Text = "  Button"
+	Detection.TextColor3 = Color3.fromRGB(200, 200, 200)
+	Detection.TextSize = 14.000
+	Detection.TextXAlignment = Enum.TextXAlignment.Left
+
+	DetectionUICorner.CornerRadius = UDim.new(0, 2)
+	DetectionUICorner.Name = "DetectionUICorner"
+	DetectionUICorner.Parent = Detection
+
+	ButtonImage.Name = "ButtonImage"
+	ButtonImage.Parent = TemplateButton
+	ButtonImage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	ButtonImage.BackgroundTransparency = 1.000
+	ButtonImage.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	ButtonImage.BorderSizePixel = 0
+	ButtonImage.Position = UDim2.new(0.827777803, 0, 0, 0)
+	ButtonImage.Size = UDim2.new(0, 31, 0, 30)
+	ButtonImage.ScaleType = Enum.ScaleType.Fit
+
+	ButtonImageUiCorner.CornerRadius = UDim.new(0, 2)
+	ButtonImageUiCorner.Name = "ButtonImageUiCorner"
+	ButtonImageUiCorner.Parent = ButtonImage
 
 	MinimizeButton.Activated:Connect(function()
 		Minimized = not Minimized
@@ -281,7 +331,7 @@ function Library:Window( args )
 			openTween:Play()
 		end
 	end)
-	
+
 	CloseButton.Activated:Connect(function()
 		for i,v in pairs(MainFrame:GetDescendants()) do
 			if v.Parent == Topbar then
@@ -295,7 +345,7 @@ function Library:Window( args )
 			end
 		end
 		minimizeTween:Play()
-		
+
 		minimizeTween.Completed:Connect(function()
 			MainFrame.Transparency = 1
 			for i,v in pairs(MainFrame:GetDescendants()) do
@@ -312,78 +362,160 @@ function Library:Window( args )
 
 	MakeDraggable(Topbar, MainFrame)
 
-	function This:Tab( args )
+	function This:Tab(args)
 		args = Library:Validate({
 			Text = "Tab",
 			Icon = "rbxassetid://15166889857"
 		}, args or {})
 
 		local Tab = {
-            Hover = false,
-            Active = false
-        }
+			Hover = false,
+			Active = false
+		}
 
 		local TabButton = TemplateTabButton:Clone()
+		local TabFrame = Template:Clone()
 		local Icon = TabButton.TabButtonImage
 
 		TabButton.Text = args.Text
 		TabButton.Name = args.Text
+
+		TabFrame.Name = "Tab "..args.Text
 		Icon.Image = args.Icon
 		Icon.Size = args.Size or UDim2.new(0, 20, 0, 14)
 
 		TabButton.Parent = Navigation
+		TabFrame.Parent = Tabholder
 		TabButton.Visible = true
 
-        function Tab:Activate()
-            if not Tab.Active then
-                if This.CurrentTab ~= nil then
-                    This.CurrentTab:Deactivate()
-                end
+		function Tab:Activate()
+			if not Tab.Active then
+				if This.CurrentTab ~= nil then
+					This.CurrentTab:Deactivate()
+				end
 
-                Tab.Active = true
-                Library:tween(TabButton, {BackgroundTransparency = .8})
-                Library:tween(TabButton, {TextColor3 = Color3.fromRGB(255, 255, 255)})
-                Library:tween(Icon, {ImageColor3 = Color3.fromRGB(255, 255, 255)})
+				Tab.Active = true
+				Library:tween(TabButton, {BackgroundTransparency = .8})
+				Library:tween(TabButton, {TextColor3 = Color3.fromRGB(255, 255, 255)})
+				Library:tween(Icon, {ImageColor3 = Color3.fromRGB(255, 255, 255)})
+				TabFrame.Visible = true
 
-                This.CurrentTab = Tab
-            end
-        end
+				This.CurrentTab = Tab
+			end
+		end
 
-        function Tab:Deactivate()
-            if Tab.Active then
-                Tab.Active = false
-                Tab.Hover = false
-                Library:tween(TabButton, {TextColor3 = Color3.fromRGB(200, 200, 200)})
-                Library:tween(Icon, {ImageColor3 = Color3.fromRGB(200, 200, 200)})
-                Library:tween(TabButton, {BackgroundTransparency = 1})
-            end
-        end
+		function Tab:Deactivate()
+			if Tab.Active then
+				Tab.Active = false
+				Tab.Hover = false
+				Library:tween(TabButton, {TextColor3 = Color3.fromRGB(200, 200, 200)})
+				Library:tween(Icon, {ImageColor3 = Color3.fromRGB(200, 200, 200)})
+				Library:tween(TabButton, {BackgroundTransparency = 1})
 
-        TabButton.MouseEnter:Connect(function()
-            Tab.Hover = true
+				TabFrame.Visible = false
+			end
+		end
 
-            if not Tab.Active then
-                Library:tween(TabButton, {TextColor3 = Color3.fromRGB(255, 255, 255)})
-                Library:tween(Icon, {ImageColor3 = Color3.fromRGB(255, 255, 255)})
-            end
-        end)
-        TabButton.MouseLeave:Connect(function()
-            Tab.Hover = false
+		TabButton.MouseEnter:Connect(function()
+			Tab.Hover = true
 
-            if not Tab.Active then
-                Library:tween(TabButton, {TextColor3 = Color3.fromRGB(200, 200, 200)})
-                Library:tween(Icon, {ImageColor3 = Color3.fromRGB(200, 200, 200)})
-            end
-        end)
+			if not Tab.Active then
+				Library:tween(TabButton, {TextColor3 = Color3.fromRGB(255, 255, 255)})
+				Library:tween(Icon, {ImageColor3 = Color3.fromRGB(255, 255, 255)})
+			end
+		end)
+		TabButton.MouseLeave:Connect(function()
+			Tab.Hover = false
 
-        TabButton.Activated:Connect(function()
-            Tab:Activate()
-        end)
+			if not Tab.Active then
+				Library:tween(TabButton, {TextColor3 = Color3.fromRGB(200, 200, 200)})
+				Library:tween(Icon, {ImageColor3 = Color3.fromRGB(200, 200, 200)})
+			end
+		end)
 
-        if This.CurrentTab == nil then
-            Tab:Activate()
-        end
-		
+		TabButton.Activated:Connect(function()
+			Tab:Activate()
+		end)
+
+		if This.CurrentTab == nil then
+			Tab:Activate()
+		end
+
+		function Tab:Button(args)
+			args = Library:Validate({
+				Text = "Button",
+				Callback = function() end
+			}, args or {})
+
+			local Button = {
+				Hover = false,
+				MouseDown = false
+			}
+
+			local RenderedButton = TemplateButton:Clone()
+			RenderedButton.Parent = TabFrame 
+			local Visual = RenderedButton.Detection
+
+			Visual.Text = "  "..args.Text
+			RenderedButton.Visible = true
+
+			function Button:SetText(text)
+				Visual.Text = tostring(text)
+			end
+
+			function Button:SetCallback(func)
+				args.Callback = func
+			end
+
+			RenderedButton.MouseEnter:Connect(function()
+				Button.Hover = true
+
+				if not Button.MouseDown then
+					Library:tween(RenderedButton, {BackgroundColor3 = Color3.fromRGB(55, 55, 55)})
+					Library:tween(Visual, {TextColor3 = Color3.fromRGB(255, 255, 255)})
+				end
+			end)
+
+			RenderedButton.MouseLeave:Connect(function()
+				Button.Hover = false
+
+				if not Button.MouseDown then
+					Library:tween(RenderedButton, {BackgroundColor3 = Color3.fromRGB(50, 50, 50)})
+					Library:tween(Visual, {TextColor3 = Color3.fromRGB(200, 200, 200)})
+				end
+			end)
+
+			UserInputService.InputBegan:Connect(function(input, gpe)	
+				if input.UserInputType == Enum.UserInputType.MouseButton1 and Button.Hover then
+					Button.MouseDown = true
+					Library:tween(RenderedButton, {BackgroundColor3 = Color3.fromRGB(70, 70, 70)})
+
+					args.Callback()
+				end
+			end)
+
+			UserInputService.InputEnded:Connect(function(input, gpe)
+				if input.UserInputType == Enum.UserInputType.MouseButton1 then
+					Button.MouseDown = false
+					Library:tween(RenderedButton, {BackgroundColor3 = Color3.fromRGB(50, 50, 50)})
+				end
+			end)
+
+			return Button
+		end
+
+		function Tab:Label(args)
+			args = Library:Validate({
+				Text = "Button",
+			}, args or {})
+
+			local Label = {
+				Hover = false,
+			}
+
+			return Label
+		end
+
 		return Tab
 	end
 
@@ -395,28 +527,67 @@ return Library
 
 --[[ :EXAMPLE
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Farls-Xavier/UiLibrary/main/Library.lua"))()
-
+    
+    --Create window
     local Window = Library:Window({
-        Title = "Ui library"
+        Title = "Ui Library"
     })
 
-    Window:Tab({
-        Text = "Home",
-        Icon = "rbxassetid://13848371774"
-    })
-
-    Optional:
-
-    Window:Tab({
-        Text = "Home",
-        Icon = "rbxassetid://13848371774",
-        Size = Udim2.new(0,0,0,0) -- Image icons size for when its scaled incorrectly
-    })
-
-    For multiple tabs I recommend doing a table like this:
-
+    --Make tabs by using the tab function
     local Tabs = {
-	    ["Home"] = Window:Tab({Text = "Home", Icon = "rbxassetid://13848371774"}, UDim2.new(0, 20, 0, 20)),
-	    ["Test"] = Window:Tab({Text = "Test", Icon = ""})
+        ["Home"] = Window:Tab({
+            Text = "Home",
+            Icon = "rbxassetid://13848371774"
+        }),
+        ["Player"] = Window:Tab({
+            Text = "Player",
+            Icon = ""
+        }),
+        ["Players"] = Window:Tab({
+            Text = "Players",
+            Icon = ""
+        })
     }
+
+    --For buttons labels toggles etc
+    Tabs.Home:Button({
+        Text = "Press me!",
+        Callback = function()
+            print("Pressed")
+        end,
+    })
+
+    Tabs.Player:Toggle({
+        Text = "Toggle me!",
+        Callback = function()
+            --IDK YET
+        end,
+    })
+
+    Tabs.Players:Label({
+        Text = "Label"
+    })
+
+    Extra funcs 
+
+    Button:SetCallback(FUNCTION) Can be used in multiple ways heres an example
+    
+    [
+    	local Btn1 = Tabs.Home:Button({
+        	Text = "Press me!",
+       	 	Callback = function()
+            print("Pressed")
+        	end,
+    	})
+    	
+    	local i
+		Btn1:SetCallback(function()
+			i = i or 0
+			i += 1
+			Btn1:SetText("  Pressed "..i.." times!")
+		end)
+	]
+
+    Label:SetText("Text")
+    
 ]]
